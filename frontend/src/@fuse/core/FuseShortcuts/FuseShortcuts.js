060c236dca964a4eb1948c5a5ba3557a74fad90c
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2022-12-14 22:59:34 +0100

    star from topbar deleted

--- frontend/src/@fuse/core/FuseShortcuts/FuseShortcuts.js
+++ frontend/src/@fuse/core/FuseShortcuts/FuseShortcuts.js
@@ -140,22 +140,22 @@ function FuseShortcuts(props) {
                 )
             )}
 
-            <Tooltip
-              title="Click to add/remove shortcut"
-              placement={props.variant === 'horizontal' ? 'bottom' : 'left'}
-            >
-              <IconButton
-                component={motion.div}
-                variants={item}
-                className="w-40 h-40 p-0"
-                aria-owns={addMenu ? 'add-menu' : null}
-                aria-haspopup="true"
-                onClick={addMenuClick}
-                size="large"
-              >
-                <FuseSvgIcon sx={{ color: amber[600] }}>heroicons-solid:star</FuseSvgIcon>
-              </IconButton>
-            </Tooltip>
+            {/*<Tooltip*/}
+            {/*  title="Click to add/remove shortcut"*/}
+            {/*  placement={props.variant === 'horizontal' ? 'bottom' : 'left'}*/}
+            {/*>*/}
+            {/*  <IconButton*/}
+            {/*    component={motion.div}*/}
+            {/*    variants={item}*/}
+            {/*    className="w-40 h-40 p-0"*/}
+            {/*    aria-owns={addMenu ? 'add-menu' : null}*/}
+            {/*    aria-haspopup="true"*/}
+            {/*    onClick={addMenuClick}*/}
+            {/*    size="large"*/}
+            {/*  >*/}
+            {/*    <FuseSvgIcon sx={{ color: amber[600] }}>heroicons-solid:star</FuseSvgIcon>*/}
+            {/*  </IconButton>*/}
+            {/*</Tooltip>*/}
           </motion.div>
         );
       }, [addMenu, props.variant, shortcutItems])}
