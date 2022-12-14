aaada59fb036ea314e9a18b0b77a2412991d5d84
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2022-12-14 20:01:39 +0100

    Removed uneccessary code

--- frontend/src/app/main/sign-in/SignInPage.js
+++ frontend/src/app/main/sign-in/SignInPage.js
@@ -1,17 +1,10 @@
 import {yupResolver} from '@hookform/resolvers/yup';
 import {Controller, useForm} from 'react-hook-form';
 import Button from '@mui/material/Button';
-import Checkbox from '@mui/material/Checkbox';
-import FormControl from '@mui/material/FormControl';
-import FormControlLabel from '@mui/material/FormControlLabel';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
-import {Link} from 'react-router-dom';
 import * as yup from 'yup';
 import _ from '@lodash';
-import FuseSvgIcon from '@fuse/core/FuseSvgIcon';
-import AvatarGroup from '@mui/material/AvatarGroup';
-import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import {useEffect} from 'react';
@@ -122,25 +115,6 @@ function SignInPage() {
                             )}
                         />
 
-                        {/*<div className="flex flex-col sm:flex-row items-center justify-center sm:justify-between">*/}
-                        {/*    <Controller*/}
-                        {/*        name="remember"*/}
-                        {/*        control={control}*/}
-                        {/*        render={({field}) => (*/}
-                        {/*            <FormControl>*/}
-                        {/*                <FormControlLabel*/}
-                        {/*                    label="Remember me"*/}
-                        {/*                    control={<Checkbox size="small" {...field} />}*/}
-                        {/*                />*/}
-                        {/*            </FormControl>*/}
-                        {/*        )}*/}
-                        {/*    />*/}
-
-                        {/*    <Link className="text-md font-medium" to="/pages/auth/forgot-password">*/}
-                        {/*        Forgot password?*/}
-                        {/*    </Link>*/}
-                        {/*</div>*/}
-
                         <Button
                             variant="contained"
                             color="secondary"
@@ -153,110 +127,27 @@ function SignInPage() {
                             Sign in
                         </Button>
 
-                        {/*<div className="flex items-center mt-32">*/}
-                        {/*    <div className="flex-auto mt-px border-t"/>*/}
-                        {/*    <Typography className="mx-8" color="text.secondary">*/}
-                        {/*        Or continue with*/}
-                        {/*    </Typography>*/}
-                        {/*    <div className="flex-auto mt-px border-t"/>*/}
-                        {/*</div>*/}
-
-                        {/*<div className="flex items-center mt-32 space-x-16">*/}
-                        {/*    <Button variant="outlined" className="flex-auto">*/}
-                        {/*        <FuseSvgIcon size={20} color="action">*/}
-                        {/*            feather:facebook*/}
-                        {/*        </FuseSvgIcon>*/}
-                        {/*    </Button>*/}
-                        {/*    <Button variant="outlined" className="flex-auto">*/}
-                        {/*        <FuseSvgIcon size={20} color="action">*/}
-                        {/*            feather:twitter*/}
-                        {/*        </FuseSvgIcon>*/}
-                        {/*    </Button>*/}
-                        {/*    <Button variant="outlined" className="flex-auto">*/}
-                        {/*        <FuseSvgIcon size={20} color="action">*/}
-                        {/*            feather:github*/}
-                        {/*        </FuseSvgIcon>*/}
-                        {/*    </Button>*/}
-                        {/*</div>*/}
                     </form>
                 </div>
             </Paper>
 
             <Box
                 className="relative hidden md:flex flex-auto items-center justify-center h-full p-64 lg:px-112 overflow-hidden"
-                sx={{backgroundImage: 'url(/assets/images/demo-content/signin_wallpaper.png)', backgroundPosition: 'center'}}
+                sx={{
+                    backgroundImage: 'url(/assets/images/demo-content/signin_wallpaper.png)',
+                    backgroundPosition: 'center'
+                }}
             >
-                {/*<svg*/}
-                {/*    className="absolute inset-0 pointer-events-none"*/}
-                {/*    viewBox="0 0 960 540"*/}
-                {/*    width="100%"*/}
-                {/*    height="100%"*/}
-                {/*    preserveAspectRatio="xMidYMax slice"*/}
-                {/*    xmlns="http://www.w3.org/2000/svg"*/}
-                {/*>*/}
-                {/*    <Box*/}
-                {/*        component="g"*/}
-                {/*        sx={{color: 'primary.light'}}*/}
-                {/*        className="opacity-20"*/}
-                {/*        fill="none"*/}
-                {/*        stroke="currentColor"*/}
-                {/*        strokeWidth="100"*/}
-                {/*    >*/}
-                {/*        <circle r="234" cx="196" cy="23"/>*/}
-                {/*        <circle r="234" cx="790" cy="491"/>*/}
-                {/*    </Box>*/}
-                {/*</svg>*/}
-                {/*<Box*/}
-                {/*    component="svg"*/}
-                {/*    className="absolute -top-64 -right-64 opacity-20"*/}
-                {/*    sx={{color: 'primary.light'}}*/}
-                {/*    viewBox="0 0 220 192"*/}
-                {/*    width="220px"*/}
-                {/*    height="192px"*/}
-                {/*    fill="none"*/}
-                {/*>*/}
-                {/*    <defs>*/}
-                {/*        <pattern*/}
-                {/*            id="837c3e70-6c3a-44e6-8854-cc48c737b659"*/}
-                {/*            x="0"*/}
-                {/*            y="0"*/}
-                {/*            width="20"*/}
-                {/*            height="20"*/}
-                {/*            patternUnits="userSpaceOnUse"*/}
-                {/*        >*/}
-                {/*            <rect x="0" y="0" width="4" height="4" fill="currentColor"/>*/}
-                {/*        </pattern>*/}
-                {/*    </defs>*/}
-                {/*    <rect width="220" height="192" fill="url(#837c3e70-6c3a-44e6-8854-cc48c737b659)"/>*/}
-                {/*</Box>*/}
-
                 <div className="z-10 relative w-full max-w-2xl">
                     <div className="text-7xl text-right font-bold leading-none text-black">
                         <div>Welcome to</div>
                         <div>our cinema</div>
                     </div>
                     <div className="mt-24 text-lg text-right tracking-tight leading-6 text-black">
-                        Following the modern times and adapting the film<br/>industry to today's digital devices, Movieland
+                        Following the modern times and adapting the film<br/>industry to today's digital devices,
+                        Movieland
                         Cinema<br/>offers you unforgettable film experiences.
                     </div>
-                    {/*<div className="flex items-center mt-32">*/}
-                    {/*    <AvatarGroup*/}
-                    {/*        sx={{*/}
-                    {/*            '& .MuiAvatar-root': {*/}
-                    {/*                borderColor: 'primary.main',*/}
-                    {/*            },*/}
-                    {/*        }}*/}
-                    {/*    >*/}
-                    {/*        <Avatar src="assets/images/avatars/female-18.jpg"/>*/}
-                    {/*        <Avatar src="assets/images/avatars/female-11.jpg"/>*/}
-                    {/*        <Avatar src="assets/images/avatars/male-09.jpg"/>*/}
-                    {/*        <Avatar src="assets/images/avatars/male-16.jpg"/>*/}
-                    {/*    </AvatarGroup>*/}
-
-                    {/*    <div className="ml-16 font-medium tracking-tight text-gray-400">*/}
-                    {/*        More than 17k people joined us, it's your turn*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
                 </div>
             </Box>
         </div>
