763c3498b708a66d0169e17f101aadb623cd6c6b
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2022-12-15 23:13:54 +0100

    Navigation Menu

--- frontend/src/app/configs/navigationConfig.js
+++ frontend/src/app/configs/navigationConfig.js
@@ -6,6 +6,36 @@ const navigationConfig = [
     icon: 'heroicons-outline:chart-bar',
     url: 'dashboard',
   },
+  {
+    id: 'settings',
+    title: 'Settings',
+    subtitle: 'Manage the operation of the cinema',
+    type: 'group',
+    icon: 'heroicons-outline:home',
+    children: [
+      {
+        id: 'settings.movies',
+        title: 'Movies',
+        type: 'item',
+        icon: 'heroicons-outline:video-camera',
+        url: '/settings/movies',
+      },
+      {
+        id: 'settings.tickets',
+        title: 'Tickets',
+        type: 'item',
+        icon: 'heroicons-outline:ticket',
+        url: '/settings/tickets',
+      },
+      {
+        id: 'settings.users',
+        title: 'Users',
+        type: 'item',
+        icon: 'heroicons-outline:user-group',
+        url: '/settings/users',
+      },
+    ],
+  },
 ];
 
 export default navigationConfig;
