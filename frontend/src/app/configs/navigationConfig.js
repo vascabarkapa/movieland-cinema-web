5f0df3efac9b9c6476977dc23da29169404b1c44
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2022-12-15 22:31:56 +0100

    Created Dashboard page

--- frontend/src/app/configs/navigationConfig.js
+++ frontend/src/app/configs/navigationConfig.js
@@ -1,20 +1,10 @@
-import i18next from 'i18next';
-import ar from './navigation-i18n/ar';
-import en from './navigation-i18n/en';
-import tr from './navigation-i18n/tr';
-
-i18next.addResourceBundle('en', 'navigation', en);
-i18next.addResourceBundle('tr', 'navigation', tr);
-i18next.addResourceBundle('ar', 'navigation', ar);
-
 const navigationConfig = [
   {
-    id: 'example-component',
-    title: 'Example',
-    translate: 'EXAMPLE',
+    id: 'dashboard-component',
+    title: 'Dashboard',
     type: 'item',
     icon: 'heroicons-outline:star',
-    url: 'example',
+    url: 'dashboard',
   },
 ];
 
