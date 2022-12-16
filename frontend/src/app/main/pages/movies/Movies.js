1bb8bdc09b17390a7fa6fc92abb879f06fa54a51
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2022-12-16 18:19:43 +0100

    Components for dashboard pages

---
+++ frontend/src/app/main/pages/movies/Movies.js
@@ -0,0 +1,37 @@
+import {styled} from '@mui/material/styles';
+import FusePageSimple from '@fuse/core/FusePageSimple';
+
+const Root = styled(FusePageSimple)(({theme}) => ({
+    // '& .FusePageSimple-header': {
+    //     backgroundColor: theme.palette.background.paper,
+    //     borderBottomWidth: 1,
+    //     borderStyle: 'solid',
+    //     borderColor: theme.palette.divider,
+    // },
+    '& .FusePageSimple-toolbar': {},
+    '& .FusePageSimple-content': {},
+    '& .FusePageSimple-sidebarHeader': {},
+    '& .FusePageSimple-sidebarContent': {},
+}));
+
+function MoviesPage(props) {
+    return (
+        <Root
+            // header={
+            //     <div className="p-24">
+            //         <h4>{t('TITLE')}</h4>
+            //     </div>
+            // }
+            content={
+                <div className="p-24">
+                    <h4>Movies</h4>
+                    <br/>
+                    <b>Coming soon.</b>
+                </div>
+            }
+            scroll="content"
+        />
+    );
+}
+
+export default MoviesPage;
