d68ac9c6d97698182893ade02c61e6d0102a09a7
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2022-12-17 13:35:55 +0100

    Custom delete confirmation modal

---
+++ frontend/src/app/shared/components/ConfirmationDeleteModal.js
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+const ConfirmationDeleteModal = ({open, setOpen, message}) => {
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <div>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {"Delete confirmation"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        {message}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>
+                        Cancel
+                    </Button>
+                    <Button color="error" onClick={handleClose}>
+                        Delete
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+}
+
+export default ConfirmationDeleteModal;
