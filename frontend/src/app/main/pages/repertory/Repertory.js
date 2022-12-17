6b654370c059501f96a07cd5e533c0e3bafda1cc
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2022-12-17 15:05:14 +0100

    Repertory table with header and options

--- frontend/src/app/main/pages/repertory/Repertory.js
+++ frontend/src/app/main/pages/repertory/Repertory.js
@@ -1,36 +1,140 @@
-import {styled} from '@mui/material/styles';
-import FusePageSimple from '@fuse/core/FusePageSimple';
+import {Paper, Rating, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
+import Button from "@mui/material/Button";
+import _ from "@lodash";
+import FuseSvgIcon from "@fuse/core/FuseSvgIcon";
+import ConfirmationDeleteModal from "../../../shared/components/ConfirmationDeleteModal";
+import * as React from "react";
+import Tooltip from "@mui/material/Tooltip";
+import RepertoryHeader from "./components/RepertoryHeader";
 
-const Root = styled(FusePageSimple)(({theme}) => ({
-    // '& .FusePageSimple-header': {
-    //     backgroundColor: theme.palette.background.paper,
-    //     borderBottomWidth: 1,
-    //     borderStyle: 'solid',
-    //     borderColor: theme.palette.divider,
-    // },
-    '& .FusePageSimple-toolbar': {},
-    '& .FusePageSimple-content': {},
-    '& .FusePageSimple-sidebarHeader': {},
-    '& .FusePageSimple-sidebarContent': {},
-}));
+// const Root = styled(FusePageSimple)(({theme}) => ({
+//     // '& .FusePageSimple-header': {
+//     //     backgroundColor: theme.palette.background.paper,
+//     //     borderBottomWidth: 1,
+//     //     borderStyle: 'solid',
+//     //     borderColor: theme.palette.divider,
+//     // },
+//     '& .FusePageSimple-toolbar': {},
+//     '& .FusePageSimple-content': {},
+//     '& .FusePageSimple-sidebarHeader': {},
+//     '& .FusePageSimple-sidebarContent': {},
+// }));
+//
+// function MoviesPage(props) {
+//     return (
+//         <Root
+//             // header={
+//             //     <div className="p-24">
+//             //         <h4>{t('TITLE')}</h4>
+//             //     </div>
+//             // }
+//             content={
+//                 <div className="p-24">
+//                     <h4>Movies</h4>
+//                     <br/>
+//                     <b>Coming soon.</b>
+//                 </div>
+//             }
+//             scroll="content"
+//         />
+//     );
+// }
 
-function RepertoryPage(props) {
+function createData(name, calories, fat, carbs, protein) {
+    return {name, calories, fat, carbs, protein};
+}
+
+const rows = [
+    createData('The Big Short', 'asdsadsdsadsad', 'asdsadsdsadsad', 'asdsadsdsadsad', 'asdsadsdsadsad'),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+];
+
+function RepertoryPage() {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
     return (
-        <Root
-            // header={
-            //     <div className="p-24">
-            //         <h4>{t('TITLE')}</h4>
-            //     </div>
-            // }
-            content={
-                <div className="p-24">
-                    <h4>Repertory</h4>
-                    <br/>
-                    <b>Coming soon.</b>
-                </div>
-            }
-            scroll="content"
-        />
+        <div className="p-36">
+            <RepertoryHeader/>
+            <TableContainer component={Paper}>
+                <Table sx={{minWidth: 650}}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell><b>Movie</b></TableCell>
+                            <TableCell><b>Price</b></TableCell>
+                            <TableCell><b>Date and time</b></TableCell>
+                            <TableCell><b>Tickets left</b></TableCell>
+                            <TableCell></TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map((row) => (
+                            <TableRow
+                                key={row.name}
+                                sx={{'&:last-child td, &:last-child th': {border: 0}}}
+                                className="hover:bg-gray-100"
+                            >
+                                <TableCell component="th" scope="row">
+                                    {row.name}
+                                </TableCell>
+                                <TableCell>{row.calories}</TableCell>
+                                <TableCell>{row.fat}</TableCell>
+                                <TableCell>{row.carbs}</TableCell>
+                                <TableCell style={{display: "flex", justifyContent: "right"}}>
+                                    <Tooltip title="View" placement="top">
+                                        <Button
+                                            variant="contained"
+                                            color="primary"
+                                            type="button"
+                                            size="small"
+                                            className="mr-5 hover:bg-purple"
+                                        >
+                                            <FuseSvgIcon>
+                                                heroicons-solid:eye
+                                            </FuseSvgIcon>
+                                        </Button>
+                                    </Tooltip>
+                                    <Tooltip title="Edit" placement="top">
+                                        <Button
+                                            variant="contained"
+                                            color="primary"
+                                            type="button"
+                                            size="small"
+                                            className="mr-5 hover:bg-blue"
+                                        >
+                                            <FuseSvgIcon>
+                                                heroicons-solid:pencil-alt
+                                            </FuseSvgIcon>
+                                        </Button>
+                                    </Tooltip>
+                                    <Tooltip title="Delete" placement="top">
+                                        <Button
+                                            variant="contained"
+                                            color="primary"
+                                            type="button"
+                                            size="small"
+                                            className="hover:bg-red"
+                                            onClick={handleClickOpen}
+                                        >
+                                            <FuseSvgIcon>
+                                                heroicons-solid:trash
+                                            </FuseSvgIcon>
+                                        </Button>
+                                    </Tooltip>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            {open && <ConfirmationDeleteModal open={open} setOpen={setOpen}
+                                              message={"Are you sure you want to delete the movie from repertory?"}/>}
+        </div>
     );
 }
 
---
+++ frontend/src/app/main/pages/repertory/components/RepertoryHeader.js
@@ -0,0 +1,32 @@
+import {Typography} from "@mui/material";
+import Button from "@mui/material/Button";
+import FuseSvgIcon from "@fuse/core/FuseSvgIcon";
+
+function RepertoryHeader() {
+    return (
+        <div className="flex w-full container">
+            <div className="flex flex-col sm:flex-row flex-auto items-center sm:items-center min-w-0 mb-24 px-5">
+                <div className="flex flex-col flex-auto">
+                    <Typography className="text-3xl text-center sm:text-left font-semibold tracking-tight leading-8">
+                        Repertory
+                    </Typography>
+                    <Typography className="font-medium tracking-tight text-center sm:text-left" color="text.secondary">
+                        List of active movies in the repertoire of the cinema
+                    </Typography>
+                </div>
+                <div className="flex items-center mt-24 sm:mt-0 sm:mx-8 space-x-12">
+                    <Button
+                        className="whitespace-nowrap"
+                        variant="contained"
+                        color="secondary"
+                        startIcon={<FuseSvgIcon size={20}>material-solid:add_comment</FuseSvgIcon>}
+                    >
+                        Add movie to repertory
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default RepertoryHeader;
