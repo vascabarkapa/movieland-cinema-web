0d2eb310720c4b768c6960e356dddf077a76c040
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2022-12-21 20:20:58 +0100

    About Page

--- frontend/src/app/main/pages/about/About.js
+++ frontend/src/app/main/pages/about/About.js
@@ -1,36 +1,49 @@
 import {styled} from '@mui/material/styles';
 import FusePageSimple from '@fuse/core/FusePageSimple';
+import FuseSvgIcon from "@fuse/core/FuseSvgIcon";
+import Tooltip from "@mui/material/Tooltip";
+import GoogleMapReact from 'google-map-react';
+import InfoWindow from 'google-map-react';
+import Typography from "@mui/material/Typography";
+import {CardContent} from "@mui/material";
+import Card from "@mui/material/Card";
 
-const Root = styled(FusePageSimple)(({theme}) => ({
-    // '& .FusePageSimple-header': {
-    //     backgroundColor: theme.palette.background.paper,
-    //     borderBottomWidth: 1,
-    //     borderStyle: 'solid',
-    //     borderColor: theme.palette.divider,
-    // },
-    '& .FusePageSimple-toolbar': {},
-    '& .FusePageSimple-content': {},
-    '& .FusePageSimple-sidebarHeader': {},
-    '& .FusePageSimple-sidebarContent': {},
-}));
 
-function AboutPage(props) {
+function Marker({text}) {
     return (
-        <Root
-            // header={
-            //     <div className="p-24">
-            //         <h4>{t('TITLE')}</h4>
-            //     </div>
-            // }
-            content={
-                <div className="p-24">
-                    <h4>About Us page</h4>
-                    <br/>
-                    <b>Coming soon.</b>
-                </div>
-            }
-            scroll="content"
-        />
+        <Tooltip title={text} placement="top">
+            <FuseSvgIcon className="text-red">heroicons-outline:location-marker</FuseSvgIcon>
+        </Tooltip>
+    );
+}
+
+function AboutPage() {
+    return (
+        <>
+            <GoogleMapReact
+                bootstrapURLKeys={{
+                    key: 'AIzaSyCt3KWfhhNIKluMJPta7TLa3-zA2KEoFMM'
+                }}
+                defaultZoom={18}
+                defaultCenter={[43.736899, 18.566168]}
+            >
+                <Marker text="Movieland Cinema" lat="43.737070" lng="18.568478"/>
+            </GoogleMapReact>
+            <div className="bg-white h-288">
+                Ovdje ce ici informacije o bioskopu
+                <br/>
+                Lokacija
+                <br/>
+                Radno vijeme
+                <br/>
+                Kontakt
+                <br/>
+                Sajt
+                <br/>
+                Logo takodje
+            </div>
+        </>
+
     );
 }
 
