52d4766e4e88a84ef941a22ed7fd83775f869c63
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2023-01-17 08:26:20 +0100

    New dashboard

--- frontend/src/app/main/pages/dashboard/Dashboard.js
+++ frontend/src/app/main/pages/dashboard/Dashboard.js
@@ -1,35 +1,59 @@
-import {styled} from '@mui/material/styles';
 import FusePageSimple from '@fuse/core/FusePageSimple';
+import { styled } from '@mui/material/styles';
+import DashboardHeader from "./components/DashboardHeader";
+import MovieWidget from "./components/MovieWidget";
+import TicketWidget from "./components/TicketWidget";
+import UserWidget from "./components/UserWidget";
+import RepertoryWidget from "./components/RepertoryWidget";
+import {motion} from 'framer-motion';
 
-const Root = styled(FusePageSimple)(({theme}) => ({
-    // '& .FusePageSimple-header': {
-    //     backgroundColor: theme.palette.background.paper,
-    //     borderBottomWidth: 1,
-    //     borderStyle: 'solid',
-    //     borderColor: theme.palette.divider,
-    // },
-    '& .FusePageSimple-toolbar': {},
-    '& .FusePageSimple-content': {},
-    '& .FusePageSimple-sidebarHeader': {},
-    '& .FusePageSimple-sidebarContent': {},
+const Root = styled(FusePageSimple)(({ theme }) => ({
+    '& .FusePageSimple-header': {
+        backgroundColor: theme.palette.background.paper,
+        boxShadow: `inset 0 0 0 1px  ${theme.palette.divider}`,
+    },
 }));
 
 function DashboardPage(props) {
+    const container = {
+        show: {
+            transition: {
+                staggerChildren: 0.1,
+            },
+        },
+    };
+
+    const item = {
+        hidden: { opacity: 0, y: 20 },
+        show: { opacity: 1, y: 0 },
+    };
+
     return (
         <Root
-            // header={
-            //     <div className="p-24">
-            //         <h4>{t('TITLE')}</h4>
-            //     </div>
-            // }
+            header={<DashboardHeader />}
             content={
-                <div className="p-24">
-                    <h4>Dashboard</h4>
-                    <br/>
-                    <b>Coming soon.</b>
+                <div className="w-full p-12 pt-16 sm:pt-24 lg:ltr:pr-0 lg:rtl:pl-0">
+                    <motion.div
+                        className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-24 w-full min-w-0 p-24"
+                        variants={container}
+                        initial="hidden"
+                        animate="show"
+                    >
+                        <motion.div variants={item}>
+                            <RepertoryWidget />
+                        </motion.div>
+                        <motion.div variants={item}>
+                            <MovieWidget />
+                        </motion.div>
+                        <motion.div variants={item}>
+                            <TicketWidget />
+                        </motion.div>
+                        <motion.div variants={item}>
+                            <UserWidget />
+                        </motion.div>
+                    </motion.div>
                 </div>
             }
-            scroll="content"
         />
     );
 }
---
+++ frontend/src/app/main/pages/dashboard/components/DashboardHeader.js
@@ -0,0 +1,88 @@
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Typography from '@mui/material/Typography';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import _ from '@lodash';
+import Button from '@mui/material/Button';
+import FuseSvgIcon from '@fuse/core/FuseSvgIcon';
+import { selectUser } from 'app/store/userSlice';
+
+function DashboardHeader(props) {
+
+
+    return (
+        <div className="flex flex-col w-full px-24 sm:px-32">
+            <div className="flex flex-col sm:flex-row flex-auto sm:items-center min-w-0 my-32 sm:my-48">
+                <div className="flex flex-auto items-center min-w-0">
+                    <Avatar className="flex-0 w-64 h-64" alt="user photo" src="...">
+                        VČ
+                    </Avatar>
+                    <div className="flex flex-col min-w-0 mx-16">
+                        <Typography className="text-2xl md:text-5xl font-semibold tracking-tight leading-7 md:leading-snug truncate">
+                            Welcome back, Vasilije Čabarkapa
+                        </Typography>
+
+                        <div className="flex items-center">
+                            <FuseSvgIcon size={20} color="action">
+                                heroicons-solid:bell
+                            </FuseSvgIcon>
+                            <Typography className="mx-6 leading-6 truncate" color="text.secondary">
+                                You have 2 new messages and 15 new tasks
+                            </Typography>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex items-center mt-24 sm:mt-0 sm:mx-8 space-x-12">
+                    <Button
+                        className="whitespace-nowrap"
+                        variant="contained"
+                        color="secondary"
+                        startIcon={<FuseSvgIcon size={20}>heroicons-solid:cog</FuseSvgIcon>}
+                    >
+                        See profile
+                    </Button>
+                </div>
+            </div>
+            <div className="flex items-center">
+                <Button
+                    onClick={() => console.log('dugme')}
+                    className="flex items-center border border-solid border-b-0 rounded-t-xl rounded-b-0 h-40 px-16 text-13 sm:text-16"
+                    variant="default"
+                    sx={{
+                        backgroundColor: (theme) => theme.palette.background.default,
+                        borderColor: (theme) => theme.palette.divider,
+                    }}
+                    endIcon={
+                        <FuseSvgIcon size={20} color="action">
+                            heroicons-solid:chevron-down
+                        </FuseSvgIcon>
+                    }
+                >
+                    Informations
+                </Button>
+                {/*<Menu*/}
+                {/*    id="project-menu"*/}
+                {/*    anchorEl={selectedProject.menuEl}*/}
+                {/*    open={Boolean(selectedProject.menuEl)}*/}
+                {/*    onClose={handleCloseProjectMenu}*/}
+                {/*>*/}
+                {/*    {projects &&*/}
+                {/*        projects.map((project) => (*/}
+                {/*            <MenuItem*/}
+                {/*                key={project.id}*/}
+                {/*                onClick={(ev) => {*/}
+                {/*                    handleChangeProject(project.id);*/}
+                {/*                }}*/}
+                {/*            >*/}
+                {/*                {project.name}*/}
+                {/*            </MenuItem>*/}
+                {/*        ))}*/}
+                {/*</Menu>*/}
+            </div>
+        </div>
+    );
+}
+
+export default DashboardHeader;
