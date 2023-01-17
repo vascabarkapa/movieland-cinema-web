ecfabbe681a202f038120bff646dcad3658d1dc4
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2023-01-17 08:45:44 +0100

    repertory widget

--- frontend/src/app/main/pages/dashboard/components/RepertoryWidget.js
+++ frontend/src/app/main/pages/dashboard/components/RepertoryWidget.js
@@ -1,41 +1,26 @@
-import IconButton from '@mui/material/IconButton';
 import Paper from '@mui/material/Paper';
-import Select from '@mui/material/Select';
 import Typography from '@mui/material/Typography';
-import { memo, useState } from 'react';
-import MenuItem from '@mui/material/MenuItem';
-import FuseSvgIcon from '@fuse/core/FuseSvgIcon';
+import {memo} from 'react';
 
 function RepertoryWidget() {
-
     return (
         <Paper className="flex flex-col flex-auto shadow rounded-2xl overflow-hidden">
             <div className="flex items-center justify-between px-8 pt-12">
                 <Typography
-                    className="px-16 text-lg font-medium tracking-tight leading-6 truncate"
+                    className="px-16 py-16 text-lg font-medium tracking-tight leading-6 truncate"
                     color="text.secondary"
                 >
-                    Naslov
+                    Broj repertoara
                 </Typography>
-                <IconButton aria-label="more" size="large">
-                    <FuseSvgIcon>heroicons-outline:dots-vertical</FuseSvgIcon>
-                </IconButton>
             </div>
-            <div className="text-center mt-8">
-                <Typography className="text-7xl sm:text-8xl font-bold tracking-tight leading-none text-blue-500">
-                    Podaci
+            <div className="text-center mt-8 pb-44">
+                <Typography className="text-7xl sm:text-9xl font-bold tracking-tight leading-none text-blue-500">
+                    1
                 </Typography>
                 <Typography className="text-lg font-medium text-blue-600 dark:text-blue-500">
-                    Ime
+                    repertoar
                 </Typography>
             </div>
-            <Typography
-                className="flex items-baseline justify-center w-full mt-20 mb-24"
-                color="text.secondary"
-            >
-                <span className="truncate">Dodatno ime</span>:
-                <b className="px-8">Dodatni broj</b>
-            </Typography>
         </Paper>
     );
 }
