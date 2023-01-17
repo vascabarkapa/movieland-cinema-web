3dfc83cf17787bde4a64c7f235afacc9062f75c8
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2023-01-17 09:00:39 +0100

    user avatar

--- frontend/src/app/main/pages/dashboard/components/DashboardHeader.js
+++ frontend/src/app/main/pages/dashboard/components/DashboardHeader.js
@@ -9,7 +9,7 @@ function DashboardHeader() {
             <div className="flex flex-col sm:flex-row flex-auto sm:items-center min-w-0 my-32 sm:my-48">
                 <div className="flex flex-auto items-center min-w-0">
                     <Avatar className="flex-0 w-64 h-64" alt="user_initials">
-                        VČ
+                        TU
                     </Avatar>
                     <div className="flex flex-col min-w-0 mx-16">
                         <Typography
