7ac555781eb5a0866465c8fe240cf1f18c2f51d2
vasilije.cabarkapa <vascabarkapa@gmail.com>
2023-03-18 00:22:45 +0100

    async handler by express

--- backend/controllers/movieController.js
+++ backend/controllers/movieController.js
@@ -1,41 +1,43 @@
+const asyncHandler = require("express-async-handler");
+
 //@desc Get all movies
 //@route GET /api/movies
 //@access public
-const getMovies = (req, res) => {
+const getMovies = asyncHandler(async (req, res) => {
     res.status(200).json({message: "Get all movies"});
-};
+});
 
 //@desc Get movie by id
 //@route GET /api/movies/:id
 //@access public
-const getMovieById = (req, res) => {
+const getMovieById = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Get movie id: ${req.params.id}`});
-};
+});
 
 //@desc Create movie
 //@route POST /api/movies
 //@access public
-const createMovie = (req, res) => {
+const createMovie = asyncHandler(async (req, res) => {
     const {name, rating} = req.body;
     if (!name || !rating) {
         res.status(400);
         throw new Error("All fields are mandatory!");
     }
     res.status(201).json({message: "Create movie"});
-};
+});
 
 //@desc Update movie by id
 //@route PUT /api/movies/:id
 //@access public
-const updateMovie = (req, res) => {
+const updateMovie = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Update movie id: ${req.params.id}`});
-};
+});
 
 //@desc Delete movie by id
 //@route DELETE /api/movies/:id
 //@access public
-const deleteMovie = (req, res) => {
+const deleteMovie = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Delete movie id: ${req.params.id}`});
-};
+});
 
 module.exports = {getMovies, getMovieById, createMovie, updateMovie, deleteMovie};
