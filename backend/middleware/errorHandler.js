6763accac60e586cbc05629c2ccaee441ffd15f0
vasilije.cabarkapa <vascabarkapa@gmail.com>
2023-03-18 00:18:41 +0100

    shared constants with error handler

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -1,16 +1,31 @@
+const {constants} = require("../constants");
+
 const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 500;
 
     switch (statusCode) {
-        case 400:
+        case constants.VALIDATION_ERROR:
             res.json({title: "Validation failed", message: err.message, stackTrace: err.stack});
             break;
 
-        case 404:
+        case constants.UNAUTHORIZED:
+            res.json({title: "Unauthorized", message: err.message, stackTrace: err.stack});
+            break;
+
+        case constants.FORBIDDEN:
+            res.json({title: "Forbidden", message: err.message, stackTrace: err.stack});
+            break;
+
+        case constants.NOT_FOUND:
             res.json({title: "Not found", message: err.message, stackTrace: err.stack});
             break;
 
+        case constants.SERVER_ERROR:
+            res.json({title: "Server error", message: err.message, stackTrace: err.stack});
+            break;
+
         default:
+            console.log("No error!");
             break;
     }
 
