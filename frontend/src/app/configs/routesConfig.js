3b62dd52251ddde0ab027fac6b74eb6924f27eea
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2023-03-26 00:38:59 +0100

    main route fixed

--- frontend/src/app/configs/routesConfig.js
+++ frontend/src/app/configs/routesConfig.js
@@ -1,33 +1,36 @@
 import FuseUtils from '@fuse/utils';
 import FuseLoading from '@fuse/core/FuseLoading';
 import { Navigate } from 'react-router-dom';
-import settingsConfig from 'app/configs/settingsConfig';
 import SignInConfig from '../main/sign-in/SignInConfig';
 import SignUpConfig from '../main/sign-up/SignUpConfig';
 import SignOutConfig from '../main/sign-out/SignOutConfig';
 import Error404Page from '../main/404/Error404Page';
 import PagesConfig from "../main/pages/PagesConfig";
 
-const routeConfigs = [PagesConfig, SignOutConfig, SignInConfig, SignUpConfig];
-const access_token = localStorage.getItem("access_token");
+const ACCESS_TOKEN = localStorage.getItem("access_token");
+const routeConfigs = ACCESS_TOKEN ? [PagesConfig, SignOutConfig, SignInConfig, SignUpConfig] :
+  [SignOutConfig, SignInConfig, SignUpConfig];
 
 const routes = [
   ...FuseUtils.generateRoutesFromConfigs(routeConfigs, null),
   {
     path: '/',
-    element: access_token ? <Navigate to="/dashboard" /> : <Navigate to="/sign-in" />,
+    element: ACCESS_TOKEN ? <Navigate to="/dashboard" /> : <Navigate to="sign-in" />,
   },
   {
     path: 'loading',
     element: <FuseLoading />,
+    element: ACCESS_TOKEN ? <FuseLoading /> : <Navigate to="sign-in" />,
   },
   {
     path: '404',
     element: <Error404Page />,
+    element: ACCESS_TOKEN ? <Error404Page /> : <Navigate to="sign-in" />,
   },
   {
     path: '*',
     element: <Navigate to="404" />,
+    element: ACCESS_TOKEN ? <Navigate to="404" /> : <Navigate to="sign-in" />,
   },
 ];
 
