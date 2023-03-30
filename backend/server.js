053194dc735e19224fe162b421b14b7698b634dc
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2023-03-30 19:08:21 +0200

    added route for users

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use(cors({
 app.use(express.json());
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/movies", require("./routes/movieRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 app.use(errorHandler);
 
 app.listen(port, () => {
