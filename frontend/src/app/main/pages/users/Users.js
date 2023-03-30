313113332ee7e021dafacbb8eb6ee81380d7e4c5
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2023-03-30 20:13:30 +0200

    user table, get users

--- frontend/src/app/main/pages/users/Users.js
+++ frontend/src/app/main/pages/users/Users.js
@@ -1,71 +1,107 @@
-import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import Button from "@mui/material/Button";
 import FuseSvgIcon from "@fuse/core/FuseSvgIcon";
 import ConfirmationDeleteModal from "../../../shared/components/ConfirmationDeleteModal";
-import * as React from "react";
+import { useEffect, useState } from "react";
 import Tooltip from "@mui/material/Tooltip";
 import UsersHeader from "./components/UsersHeader";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import UsersDetailsModal from "./components/UsersDetailsModal";
-
-function createData(name, calories, fat, carbs, protein) {
-    return {name, calories, fat, carbs, protein};
-}
-
-const rows = [
-    createData('The Big Short', 'asdsadsdsadsad', 'asdsadsdsadsad', 'asdsadsdsadsad', 'asdsadsdsadsad'),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Frozsen yoghurt', 159, 6.0, 24, 4.0),
-];
+import { showMessage } from "app/store/fuse/messageSlice";
+import { useDispatch } from 'react-redux';
+import UserService from "src/app/shared/services/user-service";
+import FuseLoading from "@fuse/core/FuseLoading";
 
 function UsersPage() {
+    const dispatch = useDispatch();
     const navigate = useNavigate();
-    const [openDeleteModal, setOpenDeleteModal] = React.useState(false);
-    const [openUsersDetailsModal, setOpenUsersDetailsModal] = React.useState(false);
+    const [openDeleteModal, setOpenDeleteModal] = useState(false);
+    const [openDetailsModal, setOpenDetailsModal] = useState(false);
+    const [isLoading, setIsloading] = useState(false);
+    const [users, setUsers] = useState([]);
+    const [tempUsers, setTempUsers] = useState([]);
+    const [userToDelete, setUserToDelete] = useState({});
+    const [userToShow, setUserToShow] = useState({});
+    const [trigger, setTrigger] = useState(false);
+    const [page, setPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(0);
 
-    const handleDeleteModalOpen = () => {
+    let pageSize = 10;
+    let startIndex = (page - 1) * pageSize;
+    let endIndex = startIndex + pageSize;
+
+
+    useEffect(() => {
+        setIsloading(true);
+        UserService.getUsers().then((response) => {
+            if (response) {
+                setUsers(response?.data);
+                setTempUsers(response?.data)?.slice(startIndex, endIndex);
+                setIsloading(false);
+                setTotalPages(Math.ceil(response?.data?.length / pageSize));
+            }
+        })
+    }, [trigger]);
+
+    useEffect(() => {
+        setTempUsers(users?.slice(startIndex, endIndex));
+    }, [page]);
+
+    const handleChangePage = (event, value) => {
+        setPage(value);
+    };
+
+    function handleOpenDeleteModal(user) {
+        setUserToDelete(user);
         setOpenDeleteModal(true);
     };
 
-    const handleUsersDetailsModalOpen = () => {
-        setOpenUsersDetailsModal(true);
+    function handleOpenDetailsModal(user) {
+        setUserToShow(user);
+        setOpenDetailsModal(true);
     };
 
-    const handleOnEdit = () => {
-        navigate("/settings/users/create");
+    function handleEditUser(id) {
+        navigate("/settings/users/edit/" + id);
     };
+
+    const handleDelete = () => {
+        UserService.deleteUser(userToDelete?._id).then((response) => {
+            if (response) {
+                setOpenDeleteModal(false);
+                dispatch(showMessage({ message: "Successfully deleted!" }));
+                setTrigger(!trigger);
+                setPage(1);
+            }
+        })
+    }
 
     return (
         <div className="p-36">
-            <UsersHeader/>
-            <TableContainer component={Paper}>
-                <Table sx={{minWidth: 650}}>
+            <UsersHeader />
+            {!isLoading ? <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }}>
                     <TableHead>
                         <TableRow>
                             <TableCell><b>Username</b></TableCell>
                             <TableCell><b>Full Name</b></TableCell>
                             <TableCell><b>Email</b></TableCell>
-                            <TableCell><b>Phone number</b></TableCell>
                             <TableCell></TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {rows.map((row) => (
+                        {tempUsers?.length > 0 ? tempUsers.map((user) => (
                             <TableRow
-                                key={row.name}
-                                sx={{'&:last-child td, &:last-child th': {border: 0}}}
+                                key={user?._id}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                                 className="hover:bg-gray-100"
                             >
                                 <TableCell component="th" scope="row">
-                                    {row.name}
+                                    {user?.username}
                                 </TableCell>
-                                <TableCell>{row.calories}</TableCell>
-                                <TableCell>{row.fat}</TableCell>
-                                <TableCell>{row.carbs}</TableCell>
-                                <TableCell style={{display: "flex", justifyContent: "right"}}>
+                                <TableCell>{user?.first_name + " " + user?.last_name}</TableCell>
+                                <TableCell>{user?.email}</TableCell>
+                                <TableCell style={{ display: "flex", justifyContent: "right" }}>
                                     <Tooltip title="View" placement="top">
                                         <Button
                                             variant="contained"
@@ -73,7 +109,7 @@ function UsersPage() {
                                             type="button"
                                             size="small"
                                             className="mr-5 hover:bg-purple"
-                                            onClick={handleUsersDetailsModalOpen}
+                                            onClick={() => handleOpenDetailsModal(user)}
                                         >
                                             <FuseSvgIcon>
                                                 heroicons-solid:eye
@@ -87,7 +123,7 @@ function UsersPage() {
                                             type="button"
                                             size="small"
                                             className="mr-5 hover:bg-blue"
-                                            onClick={handleOnEdit}
+                                            onClick={() => handleEditUser(user._id)}
                                         >
                                             <FuseSvgIcon>
                                                 heroicons-solid:pencil-alt
@@ -101,7 +137,7 @@ function UsersPage() {
                                             type="button"
                                             size="small"
                                             className="hover:bg-red"
-                                            onClick={handleDeleteModalOpen}
+                                            onClick={() => handleOpenDeleteModal(user)}
                                         >
                                             <FuseSvgIcon>
                                                 heroicons-solid:trash
@@ -110,13 +146,26 @@ function UsersPage() {
                                     </Tooltip>
                                 </TableCell>
                             </TableRow>
-                        ))}
+                        )) : <TableRow
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            className="hover:bg-gray-100"
+                        >
+                            <TableCell colSpan={6} className="text-center" component="th" scope="row">
+                                No users available
+                            </TableCell></TableRow>}
                     </TableBody>
+                    {users?.length > 10 && <TableFooter>
+                        <TableRow>
+                            <TableCell colSpan={6} className="text-center" component="th" scope="row">
+                                <Pagination count={totalPages} page={page} onChange={handleChangePage} color="secondary" />
+                            </TableCell>
+                        </TableRow>
+                    </TableFooter>}
                 </Table>
-            </TableContainer>
-            {openUsersDetailsModal && <UsersDetailsModal open={openUsersDetailsModal} setOpen={setOpenUsersDetailsModal}/>}
+            </TableContainer> : <FuseLoading />}
+            {openDetailsModal && <UsersDetailsModal open={openDetailsModal} setOpen={setOpenDetailsModal} user={userToShow} />}
             {openDeleteModal && <ConfirmationDeleteModal open={openDeleteModal} setOpen={setOpenDeleteModal}
-                                                         message={"Are you sure you want to delete the user?"}/>}
+                message={"Are you sure you want to delete the user?"} onConfirm={handleDelete} />}
         </div>
     );
 }
