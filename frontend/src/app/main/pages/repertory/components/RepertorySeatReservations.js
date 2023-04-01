63c80250d490a8f58038671b279c06ac435ce420
vasilije.cabarkapa <vascabarkapa@gmail.com>
2023-04-01 15:13:19 +0200

    view movie on repertory

--- frontend/src/app/main/pages/repertory/components/RepertorySeatReservations.js
+++ frontend/src/app/main/pages/repertory/components/RepertorySeatReservations.js
@@ -2,13 +2,34 @@ import {Paper, Typography} from "@mui/material";
 import Button from "@mui/material/Button";
 import FuseSvgIcon from "@fuse/core/FuseSvgIcon";
 import * as React from "react";
-import {useNavigate} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import RepertoryService from "../../../../shared/services/repertory-service";
+import FuseLoading from "@fuse/core/FuseLoading";
 
 const RepertorySeatReservations = () => {
-    const navigate = useNavigate();
     const rows = [];
+    const navigate = useNavigate();
+    const repertoryId = useParams().id;
+    const [isLoading, setIsLoading] = useState(false);
+    const [movieRepertory, setMovieRepertory] = useState({});
+    let seatsLeft;
+
+    useEffect(() => {
+        setIsLoading(true);
+        RepertoryService.getMovieByIdFromRepertory(repertoryId).then((response) => {
+            if (response) {
+                setMovieRepertory(response?.data);
+                seatsLeft =response?.data?.number_of_tickets;
+                setIsLoading(false);
+            }
+        })
+    }, []);
 
     for (let i = 0; i < 20; i++) {
+        if (seatsLeft > 0) {
+            seatsLeft--;
+        }
         rows.push(<Paper key={i}
                          className="inline-block w-10 h-10 md:w-32 md:h-32 sm:w-24 sm:h-24 mr-5 mb-5 rounded-0 cursor-pointer"/>);
     }
@@ -17,58 +38,67 @@ const RepertorySeatReservations = () => {
         navigate("/settings/repertory");
     }
 
+    function convertToDateTime(dateTime) {
+        return new Date(dateTime).toLocaleString();
+    }
+
     return (
-        <div className="p-36">
-            <div className="flex w-full container">
-                <div className="flex flex-col sm:flex-row flex-auto items-center sm:items-center min-w-0 mb-24 px-5">
-                    <div className="flex flex-col flex-auto">
-                        <Typography
-                            className="text-3xl text-center sm:text-left font-semibold tracking-tight leading-8">
-                            Movie Name
-                        </Typography>
-                        <Typography className="font-medium tracking-tight text-center sm:text-left"
-                                    color="text.secondary">
-                            12.12.2022. at 19.30h, 5.30 euros
-                        </Typography>
-                    </div>
-                    <div className="flex items-center mt-24 sm:mt-0 sm:mx-8 space-x-12">
-                        <i className="inline-block w-8 h-8 rounded mr-5 bg-green animate-ping"></i>220/220 tickets left
-                        <Button
-                            className="whitespace-nowrap"
-                            variant="contained"
-                            color="primary"
-                            startIcon={<FuseSvgIcon size={20}>heroicons-outline:arrow-left</FuseSvgIcon>}
-                            onClick={handleBackToRepository}
-                        >
-                            Back to Repertory
-                        </Button>
-                    </div>
-                </div>
-            </div>
-            <div className="w-full container flex justify-center">
-                <div className="block">
-                    <Paper
-                        className="h-10 sm:h-24 md:h-32 w-320 sm:w-2xl md:w-3xl rounded-0 text-center font-semibold uppercase flex justify-center items-center">Screen</Paper>
-                    <br/>
-                    <div className="flex justify-center">
-                        <div className="block">
-                            <div>{rows}</div>
-                            <div>{rows}</div>
-                            <div>{rows}</div>
-                            <div>{rows}</div>
-                            <div>{rows}</div>
-                            <br/>
-                            <div>{rows}</div>
-                            <div>{rows}</div>
-                            <div>{rows}</div>
-                            <div>{rows}</div>
-                            <div>{rows}</div>
-                            <div>{rows}</div>
+        <>
+            {isLoading ? <FuseLoading/> : <div className="p-36">
+                <div className="flex w-full container">
+                    <div
+                        className="flex flex-col sm:flex-row flex-auto items-center sm:items-center min-w-0 mb-24 px-5">
+                        <div className="flex flex-col flex-auto">
+                            <Typography
+                                className="text-3xl text-center sm:text-left font-semibold tracking-tight leading-8">
+                                {movieRepertory?.movie?.name}
+                            </Typography>
+                            <Typography className="font-medium tracking-tight text-center sm:text-left"
+                                        color="text.secondary">
+                                {convertToDateTime(movieRepertory?.dateTime)}, {movieRepertory?.price}&nbsp;&euro;
+                            </Typography>
+                        </div>
+                        <div className="flex items-center mt-24 sm:mt-0 sm:mx-8 space-x-12">
+                            <i className="inline-block w-8 h-8 rounded mr-5 bg-green animate-ping"></i>{movieRepertory?.number_of_tickets}/220
+                            tickets
+                            left
+                            <Button
+                                className="whitespace-nowrap"
+                                variant="contained"
+                                color="primary"
+                                startIcon={<FuseSvgIcon size={20}>heroicons-outline:arrow-left</FuseSvgIcon>}
+                                onClick={handleBackToRepository}
+                            >
+                                Back to Repertory
+                            </Button>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
+                <div className="w-full container flex justify-center">
+                    <div className="block">
+                        <Paper
+                            className="h-10 sm:h-24 md:h-32 w-320 sm:w-2xl md:w-3xl rounded-0 text-center font-semibold uppercase flex justify-center items-center">Screen</Paper>
+                        <br/>
+                        <div className="flex justify-center">
+                            <div className="block">
+                                <div>{rows}</div>
+                                <div>{rows}</div>
+                                <div>{rows}</div>
+                                <div>{rows}</div>
+                                <div>{rows}</div>
+                                <br/>
+                                <div>{rows}</div>
+                                <div>{rows}</div>
+                                <div>{rows}</div>
+                                <div>{rows}</div>
+                                <div>{rows}</div>
+                                <div>{rows}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>}
+        </>
     );
 }
 
