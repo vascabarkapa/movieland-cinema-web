96b5d071d5eef2299238c0b3a06953555aaad099
vascabarkapa <vascabarkapa@gmail.com>
2023-04-02 00:50:33 +0200

    reformat servet

--- backend/server.js
+++ backend/server.js
@@ -8,15 +8,15 @@ connectDb();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: 'http://localhost:3000'
-}));
+app.use(cors({origin: 'http://localhost:3000'}));
 app.use(express.json());
+
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/movies", require("./routes/movieRoutes"));
 app.use("/api/repertories", require("./routes/repertoryRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/dashboard", require("./routes/dashboardRoutes"));
+
 app.use(errorHandler);
 
 app.listen(port, () => {
