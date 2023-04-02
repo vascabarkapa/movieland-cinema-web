9cba56a2ee113700f7f2562d4a1812950cbe62af
vascabarkapa <vascabarkapa@gmail.com>
2023-04-03 00:14:12 +0200

    fixed dashboard count

--- backend/controllers/dashboardController.js
+++ backend/controllers/dashboardController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const Movie = require("../models/movieModel");
+const Ticket = require("../models/ticketModel");
 
 //@desc Get all number of models
 //@route GET /api/dashboard
@@ -8,8 +9,9 @@ const Movie = require("../models/movieModel");
 const getCount = asyncHandler(async (req, res) => {
     const movies = await Movie.find();
     const users = await User.find();
+    const tickets = await Ticket.find();
 
-    res.status(200).json({ repertories: 1, movies: movies?.length, tickets: 5, users: users?.length });
+    res.status(200).json({ repertories: 1, movies: movies?.length, tickets: tickets?.length, users: users?.length });
 });
 
 module.exports = { getCount };
