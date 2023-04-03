4152fb0deeb3e2968975c85d1420c42847fd3434
vascabarkapa <vascabarkapa@gmail.com>
2023-04-04 01:36:17 +0200

    not validate all ticket routes

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const { getTickets, getTicketById, createTicket } = require("../controllers/ticketController");
 const validateToken = require("../middleware/validateTokenHandler");
 
+router.route("/").post(createTicket);
+
 router.use(validateToken);
-router.route("/").get(getTickets).post(createTicket);
+router.route("/").get(getTickets);
 router.route("/:id").get(getTicketById);
 
 module.exports = router;
