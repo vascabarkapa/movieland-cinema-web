f08ea9110c7355e2237cea2990cf2300d6c3c0c0
vascabarkapa <vascabarkapa@gmail.com>
2023-04-05 00:22:41 +0200

    seat number

--- frontend/src/app/main/pages/repertory/components/Seat.js
+++ frontend/src/app/main/pages/repertory/components/Seat.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Paper } from '@mui/material';
+import { Paper, Tooltip } from '@mui/material';
 
 const NUM_ROWS = 11;
 const NUM_COLS = 20;
-const WHITE_SEAT = "inline-block w-10 h-10 md:w-32 md:h-32 sm:w-24 sm:h-24 mr-5 mb-5 rounded-0";
-const RED_SEAT = "inline-block bg-red w-10 h-10 md:w-32 md:h-32 sm:w-24 sm:h-24 mr-5 mb-5 rounded-0";
+const WHITE_SEAT = "inline-block w-10 h-10 md:w-32 md:h-32 sm:w-24 sm:h-24 mr-5 mb-5 rounded-0 cursor-pointer";
+const RED_SEAT = "inline-block bg-red w-10 h-10 md:w-32 md:h-32 sm:w-24 sm:h-24 mr-5 mb-5 rounded-0 cursor-pointer";
 
 const Seat = ({ seatsLeft }) => {
     const papers = [];
@@ -17,8 +17,10 @@ const Seat = ({ seatsLeft }) => {
             const reservedStyle = seatsLeft > 0 ? RED_SEAT : WHITE_SEAT;
 
             row.push(
-                <Paper key={index}
-                    className={reservedStyle} />
+                <Tooltip key={index} placement="top" title={"SEAT_NO_" + (index + 1)}>
+                    <Paper key={index}
+                        className={reservedStyle} />
+                </Tooltip>
             );
 
             seatsLeft--;
