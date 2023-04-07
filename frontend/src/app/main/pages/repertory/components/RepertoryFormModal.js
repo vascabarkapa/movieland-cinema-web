a473bb4f5ce7d73b1b76948218e0fbff8fdd6e8e
vascabarkapa <vascabarkapa@gmail.com>
2023-04-07 22:11:44 +0200

    added autocomplete to movies

--- frontend/src/app/main/pages/repertory/components/RepertoryFormModal.js
+++ frontend/src/app/main/pages/repertory/components/RepertoryFormModal.js
@@ -6,16 +6,17 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import * as yup from "yup";
-import {Controller, useForm} from "react-hook-form";
-import {yupResolver} from "@hookform/resolvers/yup";
+import { Controller, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 import TextField from "@mui/material/TextField";
-import {InputAdornment} from "@mui/material";
+import { Autocomplete, InputAdornment } from "@mui/material";
 import dayjs from 'dayjs';
-import {LocalizationProvider} from "@mui/x-date-pickers/LocalizationProvider";
-import {DateTimePicker} from '@mui/x-date-pickers/DateTimePicker';
-import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs';
-import {useEffect, useState} from "react";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { useEffect, useState } from "react";
 import RepertoryService from "../../../../shared/services/repertory-service";
+import MovieService from 'src/app/shared/services/movie-service';
 
 /**
  * Repertory Form Validation Schema
@@ -37,42 +38,49 @@ const schema = yup.object().shape({
         .min(1, 'Price must be greater than or equal to 1'),
 });
 
-const RepertoryFormModal = ({open, setOpen, id}) => {
+const RepertoryFormModal = ({ open, setOpen, id }) => {
     const [isLoading, setIsLoading] = useState(false);
     const [isLoaded, setIsLoaded] = useState(false);
+    const [movies, setMovies] = useState([]);
     const [editMovieRepertory, setEditMovieRepertory] = useState({});
     const [dateTimeValue, setDateTimeValue] = React.useState(null);
 
     useEffect(() => {
-        if (id) {
-            RepertoryService.getMovieByIdFromRepertory(id).then((response) => {
-                if (response) {
-                    setEditMovieRepertory(response?.data);
+        MovieService.getMovies().then((movieResponse) => {
+            if (movieResponse) {
+                setMovies(movieResponse?.data);
+
+                if (id) {
+                    RepertoryService.getMovieByIdFromRepertory(id).then((repertoryResponse) => {
+                        if (repertoryResponse) {
+                            setEditMovieRepertory(response?.data);
+                            setIsLoaded(true);
+                        }
+                    })
+                } else {
                     setIsLoaded(true);
                 }
-            })
-        } else {
-            setIsLoaded(true);
-        }
+            }
+        })
     }, [])
 
     const handleClose = () => {
         setOpen(false);
     };
 
-    const {control, formState, handleSubmit, setError, setValue} = useForm({
+    const { control, formState, handleSubmit, setError, setValue } = useForm({
         mode: 'onChange',
         resolver: yupResolver(schema),
     });
 
-    const {isValid, dirtyFields, errors} = formState;
+    const { isValid, dirtyFields, errors } = formState;
 
     function onSubmit({
-                          movie,
-                          dateTime,
-                          numberOfTickets,
-                          price
-                      }) {
+        movie,
+        dateTime,
+        numberOfTickets,
+        price
+    }) {
         console.log(
             movie,
             dateTime,
@@ -109,26 +117,35 @@ const RepertoryFormModal = ({open, setOpen, id}) => {
                                 <Controller
                                     name="movie"
                                     control={control}
-                                    render={({field}) => (
-                                        <TextField
+                                    render={({ field }) => (
+                                        <Autocomplete
                                             {...field}
                                             className="mb-24 col-span-1 md:col-span-2"
-                                            label="Movie"
-                                            type="text"
-                                            variant="outlined"
-                                            error={!!errors.movie}
-                                            helperText={errors?.movie?.message}
-                                            required
-                                            fullWidth
-                                            size="small"
+                                            id="movie"
+                                            options={movies}
+                                            getOptionLabel={(option) => option.name + " [" + option.genre + "]"}
+                                            renderInput={(params) => (
+                                                <TextField
+                                                    {...params}
+                                                    label="Movie"
+                                                    type="text"
+                                                    variant="outlined"
+                                                    error={!!errors.movie}
+                                                    helperText={errors?.movie?.message}
+                                                    required
+                                                    fullWidth
+                                                    size="small"
+                                                />
+                                            )}
                                         />
                                     )}
                                 />
 
+
                                 <Controller
                                     name="dateTime"
                                     control={control}
-                                    render={({field}) => (
+                                    render={({ field }) => (
                                         // <TextField
                                         //     {...field}
                                         //     className="mb-24"
@@ -144,10 +161,10 @@ const RepertoryFormModal = ({open, setOpen, id}) => {
                                         <LocalizationProvider dateAdapter={AdapterDayjs}>
                                             <DateTimePicker
                                                 renderInput={(props) => <TextField {...field} {...props} size="small"
-                                                                                   required
-                                                                                   error={!!errors.dateTime} fullWidth
-                                                                                   className="mb-24"
-                                                                                   helperText={errors?.dateTime?.message}/>}
+                                                    required
+                                                    error={!!errors.dateTime} fullWidth
+                                                    className="mb-24"
+                                                    helperText={errors?.dateTime?.message} />}
                                                 label="Date and Time"
                                                 value={dateTimeValue}
                                                 onChange={(newValue) => {
@@ -161,7 +178,7 @@ const RepertoryFormModal = ({open, setOpen, id}) => {
                                 <Controller
                                     name="price"
                                     control={control}
-                                    render={({field}) => (
+                                    render={({ field }) => (
                                         <TextField
                                             {...field}
                                             className="mb-24"
@@ -179,7 +196,7 @@ const RepertoryFormModal = ({open, setOpen, id}) => {
                                                         &euro;
                                                     </InputAdornment>
                                                 ),
-                                                inputProps: {min: 1, step: 0.1}
+                                                inputProps: { min: 1, step: 0.1 }
                                             }}
                                         />
                                     )}
@@ -194,7 +211,7 @@ const RepertoryFormModal = ({open, setOpen, id}) => {
                         <Button type="submit" color="secondary">
                             {!isLoading ? (id ? "Edit" : "Add") :
                                 <img height={25} width={25} src="/assets/images/logo/movieland_main.svg"
-                                     alt="movieland_cinema_loading_logo"></img>}
+                                    alt="movieland_cinema_loading_logo"></img>}
                         </Button>
                     </DialogActions>
                 </form> : <></>}
